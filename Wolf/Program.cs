61b96c92771a76ded87de9250f3e356cac003224
John Soto <scruffy403@gmail.com>
2022-02-06 12:11:50 +0000

    Add project files.

---
+++ Wolf/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+
+namespace Wolf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("   /|");
+            Console.WriteLine("  / |");
+            Console.WriteLine(" /  |");
+            Console.WriteLine("/___|");
+
+            Console.ReadLine();
+        }
+    }
+}
