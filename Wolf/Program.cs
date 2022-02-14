0f1cb64cf16c348449548795efa70345030e1bd1
John Soto <scruffy403@gmail.com>
2022-02-14 22:51:32 +0000

    simple guessing game

--- Wolf/Program.cs
+++ Wolf/Program.cs
@@ -11,15 +11,34 @@ namespace Wolf
     {
         static void Main(string[] args)
         {
-            int index = 6;
-            // do/while loop, do portion gets executed before the while condition is checked
-            do
+            string secretWord = "giraffe";
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = 3;
+            bool outOfGuesses = false;
+
+            while (guess != secretWord && !outOfGuesses)
             {
-                Console.WriteLine("The number is: " + index);
-                index++;
-            } while (index <= 5);
-
-
+                if (guessCount < guessLimit)
+                {
+                    Console.Write("Enter guess: ");
+                    guess = Console.ReadLine();
+                    guessCount++;
+                }
+                else
+                {
+                    outOfGuesses = true;
+                }
+                
+            }
+            if (outOfGuesses)
+            {
+                Console.Write("You lose!");
+            }
+            else
+            {
+                Console.Write("You win!");
+            }
 
             Console.ReadLine();
         }              
